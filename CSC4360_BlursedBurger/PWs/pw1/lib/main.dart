a3a1f83136142ac894a0f9265898ec03ca91b24d
Kush <islayusim@live.com>
2024-02-28 13:48:47 -0500

    resolve error with title and new home page in main.dart file

--- CSC4360_BlursedBurger/PWs/pw1/lib/main.dart
+++ CSC4360_BlursedBurger/PWs/pw1/lib/main.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import './common/color_extension.dart';
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'JMT',
       debugShowCheckedModeBanner: false,
       // This is the theme of your application.
@@ -33,28 +31,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       home: const MainTabView(),
-
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -143,7 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-
     );
   }
 }
