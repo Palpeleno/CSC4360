46cc78ec2c839726cf195eebf6974a8b9002de19
Kush Darji <60727944+Palpeleno@users.noreply.github.com>
2024-02-27 20:18:57 -0500

    Delete csc4360_BlursedBurger directory

--- csc4360_BlursedBurger/InclassActivety/activiety008/android/app/src/main/kotlin/com/example/activiety008/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.activiety008
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- csc4360_BlursedBurger/InclassActivety/activiety008/database_practice/android/app/src/main/kotlin/com/example/database_practice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.database_practice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
