92ca9313aae9d1b4824b51f77b949577cffb79f1
Kush Darji <60727944+Palpeleno@users.noreply.github.com>
2024-02-27 20:05:09 -0500

    Delete csc4360_BlursedBurger directory

--- csc4360_BlursedBurger/CWs/cw3/taskmanager/android/app/src/main/kotlin/com/example/taskmanager/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.taskmanager
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- csc4360_BlursedBurger/PWs/pw1/android/app/src/main/kotlin/com/example/pw1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pw1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
