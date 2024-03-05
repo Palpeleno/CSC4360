4330df964b188e08d6ac83c9720f605bbebd0592
Kush <islayusim@live.com>
2024-03-04 19:17:13 -0500

    completed assignment without building, please build apk --release

--- CSC4360_BlursedBurger/InclassActivety/activiety010_inputvali/lib/main.dart
+++ CSC4360_BlursedBurger/InclassActivety/activiety010_inputvali/lib/main.dart
@@ -1,125 +1,217 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Form Validation Demo';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: appTitle,
+      initialRoute: '/',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildNameField(),
+          buildEmailnContactField(),
+          buildPasswordField(),
+          buildSubmitButton(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Widget buildNameField() {
+    return Row(
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: 100,
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'First Name',
+                labelText: 'First Name',
+              ),
+              validator: (firstName) {
+                if (firstName == null || firstName.isEmpty) {
+                  return 'Please enter First Name';
+                }
+                return null;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        SizedBox(width: 16), // Add spacing between first and last name fields
+        Expanded(
+          child: SizedBox(
+            height: 100,
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Last Name',
+                labelText: 'Last Name',
+              ),
+              validator: (lastName) {
+                if (lastName == null || lastName.isEmpty) {
+                  return 'Please enter Last Name';
+                }
+                return null;
+              },
             ),
-          ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildEmailnContactField() {
+    return Row(children: [
+      Expanded(
+        child: SizedBox(
+          width: 200,
+          height: 100,
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Enter your email',
+              labelText: 'Email',
+            ),
+            validator: (email) {
+              //email validation logic grep
+              if (email == null || email.isEmpty) {
+                return 'Please enter your valid Email';
+              }
+              // grep email validation
+              if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                  .hasMatch(email)) {
+                return 'Enter a valid email address';
+              }
+              return null;
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      SizedBox(width: 16),
+      Expanded(
+        child: SizedBox(
+          width: 200,
+          height: 100,
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Enter your contact #',
+              labelText: 'Contact Number',
+            ),
+            validator: (contactnum) {
+              //email validation logic grep
+              if (contactnum == null || contactnum.isEmpty) {
+                return 'Please enter your valid Email';
+              }
+              if (!RegExp(
+                      r'^\+?1?\s?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$')
+                  .hasMatch(contactnum)) {
+                return 'Enter a valid contact number';
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget buildPasswordField() {
+    return SizedBox(
+      width: 300,
+      // height: 100,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Enter your password',
+          labelText: 'JohnDoe74!',
+        ),
+        validator: (password) {
+          //email validation logic grep
+          if (password == null || password.isEmpty) {
+            return 'Password must be at least 6 chracters long';
+          }
+          if (password.length < 4) {
+            return 'Password must be at least 4 characters long';
+          }
+
+          // Check if the password contains at least one digit
+          if (!RegExp(r'\d').hasMatch(password)) {
+            return 'Password must contain at least one digit';
+          }
+
+          // Check if the password contains at least one special character
+          if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password)) {
+            return 'Password must contain at least one special character';
+          }
+
+          if (password.length > 16) {
+            return 'Password must not be longer than 16 characters';
+          }
+          return null;
+        },
+      ),
     );
+  }
+
+  Widget buildSubmitButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: ElevatedButton(
+        onPressed: () {
+          onSubmitPressed();
+        },
+        child: Text('submit'),
+      ),
+    );
+  }
+
+  void onSubmitPressed() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Processing Data')),
+      );
+    }
   }
 }
