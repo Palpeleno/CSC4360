8171704da4def79e360587b397e92ee7ea251f1b
Kush <islayusim@live.com>
2024-03-08 20:14:49 -0500

    adding controller

--- CSC4360_BlursedBurger/CWs/cw5_2dracesim/lib/game/game.dart
+++ CSC4360_BlursedBurger/CWs/cw5_2dracesim/lib/game/game.dart
@@ -1,11 +1,16 @@
+import 'package:cw5_2dracesim/game/player.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flame/sprite.dart';
 
-class PalRacingGame extends FlameGame {
+class PalRacingGame extends FlameGame with PanDetector {
+  late Player player1;
+  Offset? _pointerStartPosition;
+
   final String sprite1Path = 'cars_ext/pitstop_car_2.png';
-  // final String sprite2Path = 'cars_ext/pitstop_car_5.png';
+  final String sprite2Path = 'cars_ext/pitstop_car_5.png';
 
   @override
   Future<void> onLoad() async {
@@ -19,29 +24,55 @@ class PalRacingGame extends FlameGame {
     // final sprite1_2 = await Flame.images.load('pitstop_car_2_left.png');
     // final sprite1_3 = await Flame.images.load('pitstop_car_2_right.png');
 
-    // final sprite2 = await Flame.images.load(sprite2Path);
+    final sprite2Image = await Flame.images.load(sprite2Path);
+    final sprite2 = Sprite(sprite2Image);
+
     // // final sprite2_2 = await Flame.images.load('pitstop_car_5_left.png');
     // // final sprite2_3 = await Flame.images.load('pitstop_car_5_right.png');
 
-    SpriteComponent player1 = SpriteComponent(
+    player1 = Player(
       sprite: sprite1,
-      autoResize: true,
-      scale: Vector2(.2, .2),
+      size: Vector2(64, 112),
+      position: Vector2(350, 725),
       angle: radians(180),
       anchor: Anchor.center,
     );
 
-    //sprite components
-    // final sprite1_2Component = SpriteComponent.fromImage(sprite1);
-
-    //size sprites
-    // player1.scale = Vector2(.16, .16);
-
-    // position sprites
-    player1.x = 350;
-    player1.y = 650;
+    Player player2 = Player(
+      sprite: sprite2,
+      size: Vector2(64, 112),
+      position: Vector2(250, 725),
+      angle: radians(180),
+      anchor: Anchor.center,
+    );
 
     // add sprites to game
     add(player1);
+    // add(player2);
+  }
+  // user input for movment using touch gestrure
+
+  void onPanStart(DragStartInfo info) {
+    _pointerStartPosition = info.raw.globalPosition;
+
+    print('pan start');
+  }
+
+  void onPanUpdate(DragUpdateInfo info) {
+    final pointerCurrentPostion = info.raw.globalPosition;
+
+    var delta = pointerCurrentPostion - _pointerStartPosition!;
+    player1.setMoveDirection(Vector2(delta.dx, delta.dy));
+    print('pan update');
+  }
+
+  void onPanEnd(DragEndInfo info) {
+    _pointerStartPosition = null;
+    player1.setMoveDirection(Vector2.zero());
+  }
+
+  void onPanCancel() {
+    _pointerStartPosition = null;
+    player1.setMoveDirection(Vector2.zero());
   }
 }
---
+++ CSC4360_BlursedBurger/CWs/cw5_2dracesim/lib/game/player.dart
@@ -0,0 +1,33 @@
+import 'package:flame/components.dart';
+
+class Player extends SpriteComponent {
+  Vector2 _moveDirection = Vector2.zero();
+
+  double _speed = 100;
+
+  Player({
+    Sprite? sprite,
+    Vector2? position,
+    Vector2? size,
+    double? angle,
+    Anchor? anchor,
+  }) : super(
+            sprite: sprite,
+            position: position,
+            size: size,
+            angle: angle,
+            anchor: anchor);
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // provides update of player in the general direction
+    // dt = delta time, of movement of object manipulated by lpayer speed
+    this.position += _moveDirection.normalized() * _speed * dt;
+  }
+
+  void setMoveDirection(Vector2 newMoveDirection) {
+    _moveDirection = newMoveDirection;
+  }
+}
