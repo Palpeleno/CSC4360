346386befde70e86537b386edf4803be65224f4e
Kush D <60727944+Palpeleno@users.noreply.github.com>
2024-03-10 17:34:19 -0400

    done :

--- CSC4360_BlursedBurger/Hw/hw3_cardmatching/lib/utils/game_logic.dart
+++ CSC4360_BlursedBurger/Hw/hw3_cardmatching/lib/utils/game_logic.dart
@@ -1,31 +1,31 @@
 class Game {
-  final String hiddenCardpath = 'assets/img/back_card_yugioh.jpg';
+  final String hiddenCardpath = 'assets/img/back_card_yugioh.png';
   List<String>? gameImg;
 
   // matching card faces
   final List<String> card_list = [
     // 1st set
-    "assets/img/ODST_Virgil_1.jpg",
-    "assets/img/ODST_Virgil_2.jpg",
-    "assets/img/ODST_Virgil_3.jpg",
-    "assets/img/ODST_Virgil_4.jpg",
-    "assets/img/ODST_Virgil_5.jpg",
-    "assets/img/ODST_Virgil_6.jpg",
-    "assets/img/ODST_Virgil_7.jpg",
-    "assets/img/ODST_Virgil_8.jpg",
-    "assets/img/ODST_Virgil_9.jpg",
-    "assets/img/ODST_Virgil_10.jpg",
+    "assets/img/ODST_Virgil_1.png",
+    "assets/img/ODST_Virgil_2.png",
+    "assets/img/ODST_Virgil_3.png",
+    "assets/img/ODST_Virgil_4.png",
+    "assets/img/ODST_Virgil_5.png",
+    "assets/img/ODST_Virgil_6.png",
+    "assets/img/ODST_Virgil_7.png",
+    "assets/img/ODST_Virgil_8.png",
+    "assets/img/ODST_Virgil_9.png",
+    "assets/img/ODST_Virgil_10.png",
     // 2nd  set
-    "assets/img/ODST_Virgil_1.jpg",
-    "assets/img/ODST_Virgil_2.jpg",
-    "assets/img/ODST_Virgil_3.jpg",
-    "assets/img/ODST_Virgil_4.jpg",
-    "assets/img/ODST_Virgil_5.jpg",
-    "assets/img/ODST_Virgil_6.jpg",
-    "assets/img/ODST_Virgil_7.jpg",
-    "assets/img/ODST_Virgil_8.jpg",
-    "assets/img/ODST_Virgil_9.jpg",
-    "assets/img/ODST_Virgil_10.jpg",
+    "assets/img/ODST_Virgil_1.png",
+    "assets/img/ODST_Virgil_2.png",
+    "assets/img/ODST_Virgil_3.png",
+    "assets/img/ODST_Virgil_4.png",
+    "assets/img/ODST_Virgil_5.png",
+    "assets/img/ODST_Virgil_6.png",
+    "assets/img/ODST_Virgil_7.png",
+    "assets/img/ODST_Virgil_8.png",
+    "assets/img/ODST_Virgil_9.png",
+    "assets/img/ODST_Virgil_10.png",
   ];
   // stroes the first clecked card and checks if they match
   List<Map<int, String>> matchCheck = [];
@@ -35,9 +35,6 @@ class Game {
   //init the game
 
   void initGame() {
-    // TODO
-    // gameImg = List.generate(card_list.length, (index) => hiddenCardpath);
-
     gameImg = List.generate(cardCount, (index) => hiddenCardpath);
   }
 }
